e1268efb343c6be1de70a7dcd0e710dec3d7649a
Hashan000-1 <pabasarahashan977@gmail.com>
2025-08-15 12:20:55 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: '',
-    password: '',
+    email: 'pabasarahashan977@gmail.com',
+    password: 'Nirosha#0776903322',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
